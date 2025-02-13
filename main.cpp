c243195bb959d6d69034ea6e25542a420aeb8bac
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-02-13 09:21:04 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+              int N,i,s,m,countsup;
+              cout<<"inserisci quanti numeri devo generare"<<endl;
+              cin>>N;
+              vector<int> v(N);
+              s=0;
+              for(i=0;i<N;i=i+1)
+              {
+                v(i)=rand(1001);
+                s=s+v(i);
+              }
+           m=s/N;
+           cout<<"la media è"&m;
+           countsup=0;
+           for(i=0;i<N;i=0)
+           {
+            if v(i)>m
+            {
+                cout<<"i valori superiori alla media sono" <<v(i) << "la loro posizione è"<< i;
+                countsup=countsup+1;
+            }
+           }
+           cout<<"gli elementi superiori alla media sono " <<countsup;
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
